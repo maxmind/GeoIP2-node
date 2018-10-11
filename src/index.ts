a28d757442e0ab818168e085ea5777be10eb1824
Kevin Centeno <kevin@oldeyorke.ca>
2018-10-10 20:41:11 -0400

    Add typedocs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs';
 
+/** Class representing the GeoIP2-node library **/
 class GeoIP2 {
+  /**
+   * Open an mmdb database file
+   * @param filename - The mmdb database file to open
+   */
   public static open(filename: string): Promise<any> {
     return new Promise((resolve, reject) => {
       fs.readFile(filename, (err, data) => {
