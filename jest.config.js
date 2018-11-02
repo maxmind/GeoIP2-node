6dffa807b0ea08a9ed16de54edf91fee65d03b55
Kevin Centeno <kevin@oldeyorke.ca>
2018-11-02 21:19:32 +0000

    Add coverage testing to travis

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0,
+    }
+  },
   roots: ['<rootDir>/src', '<rootDir>/__mocks__'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
