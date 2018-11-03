f435c80a7f382463111cf3c54ff60a61e4f0c2ab
Kevin Centeno <kevin@oldeyorke.ca>
2018-11-02 23:58:30 -0400

    Add maxmind prop to City model

--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,5 @@
 import mmdb = require('maxmind');
+import { ICityResponse, IMaxMind } from './types';
 
 /** Class representing the model of a "City" response **/
 export class City {
@@ -6,6 +7,7 @@ export class City {
   public readonly continent: mmdb.IContinent | undefined;
   public readonly country: mmdb.ICountry | undefined;
   public readonly location: mmdb.ILocation | undefined;
+  public readonly maxmind: IMaxMind | undefined;
   public readonly registered_country: mmdb.IBaseCountry | undefined;
   public readonly represented_country: mmdb.IRepresentedCountry | undefined;
   public readonly subdivisions: mmdb.ISubdivisions[] | undefined;
@@ -16,11 +18,12 @@ export class City {
    *
    * @param fields The GeoIP2 response
    */
-  public constructor(fields: mmdb.ICityResponse) {
+  public constructor(fields: ICityResponse) {
     this.city = fields.city;
     this.continent = fields.continent;
     this.country = fields.country;
     this.location = fields.location;
+    this.maxmind = fields.maxmind;
     this.registered_country = fields.registered_country;
     this.represented_country = fields.represented_country;
     this.subdivisions = fields.subdivisions;
