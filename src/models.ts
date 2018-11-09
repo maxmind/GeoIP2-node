7424507e3df0a8c126534544303f1352695b1866
Adam Lapczynski <alapczynski@maxmind.com>
2018-11-09 16:48:14 -0500

    Remove undefined from ip_address assignment in Asn

--- src/models.ts
+++ src/models.ts
@@ -64,6 +64,6 @@ export class Asn {
       response.autonomous_system_number || undefined;
     this.autonomous_system_organization =
       response.autonomous_system_organization || undefined;
-    this.ip_address = response.ip_address || undefined;
+    this.ip_address = response.ip_address;
   }
 }
