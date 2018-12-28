5dcc4215980a8c3e6dab377214a27aa1f1393e0e
Kevin Centeno <kevin@oldeyorke.ca>
2018-12-28 22:55:35 +0000

    Export models and records for use in minfraud api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import * as Models from './models';
 import Reader from './reader';
+import * as Records from './records';
 import WebServiceClient from './webServiceClient';
 
 export = {
+  Models,
   Reader,
+  Records,
   WebServiceClient,
 };
