2e12e5522bc4a2f5c9143746749544f17edcf793
Kevin Centeno <kevin@oldeyorke.ca>
2019-01-11 22:21:32 +0000

    Fix export of model and records interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import * as Models from './models';
 import Reader from './reader';
-import * as Records from './records';
 import WebServiceClient from './webServiceClient';
 
-export = {
-  Models,
-  Reader,
-  Records,
-  WebServiceClient,
-};
+export { Reader, WebServiceClient };
+export * from './records';
+export * from './models';
