676fb4b1e3ffc02df5523de111d587c5c80d9d15
Dallas Hogan <dallastar4@gmail.com>
2026-04-21 13:36:09 -0400

    Update for typescript v6

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
